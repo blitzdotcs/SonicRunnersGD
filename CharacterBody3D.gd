91a011bf585299aac426492c7a92953d5661b7e9
Ty <93739827+blitzexlmao@users.noreply.github.com>
2023-09-29 13:11:45 -0400

    literally the Sonic Runners update ever

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,40 +1,39 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MAX_JUMPS = 3
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camerabase = $CameraBase
 @onready var anim = $SonicMMM/AnimationPlayer
 
-func _ready():
-	Input.mouse_mode = 2
+enum CharacterState {
+	RUNNING,
+	JUMPING,
+}
+
+var current_state = CharacterState.RUNNING
+var jumps_remaining = MAX_JUMPS
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jumps_remaining > 0:
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("", "", "move_foward", "")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	if direction and is_on_floor():
+		jumps_remaining -= 1
+		current_state = CharacterState.JUMPING
+		anim.play("sn_ball")
+	elif is_on_floor():
+		current_state = CharacterState.RUNNING
 		anim.play("sn_run")
-	
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		jumps_remaining = MAX_JUMPS
+
+	var direction = -transform.basis.z
+
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
 
 	move_and_slide()
